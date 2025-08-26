6b9bbe850391b0a65ae0b0deb948c5556f21a91b
Rajeev R Sharma <i.rarsh@gmail.com>
2025-08-26 22:46:34 +0530

    chore: update dependencies and test ui-next integration

--- apps/tools/app/layout.tsx
+++ apps/tools/app/layout.tsx
@@ -1,30 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { AppLayout } from "@serp-tools/ui-next/components/app-layout";
+import type { Metadata } from "next";
 
-import "@serp-tools/ui/globals.css";
-import { Providers } from "@/components/providers";
-
-const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-});
+export const metadata: Metadata = {
+  title: "Serp Tools",
+  description: "Description of Serp Tools",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+  return <AppLayout>{children}</AppLayout>;
 }
