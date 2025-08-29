0a0b29f24e52e7fab3d6e02cdc327fb23484b46e
Rajeev R Sharma <i.rarsh@gmail.com>
2025-08-29 15:44:01 +0530

    chore: fix eslint rule for missing pages in ui-next package

--- packages/ui-next/eslint.config.js
+++ packages/ui-next/eslint.config.js
@@ -1,4 +1,13 @@
 import { nextJsConfig } from "@serp-tools/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+const config = [...nextJsConfig];
+const nextPluginConfig = config.find(
+  (c) => c.plugins && c.plugins["@next/next"]
+);
+
+if (nextPluginConfig && nextPluginConfig.rules) {
+  nextPluginConfig.rules["@next/next/no-html-link-for-pages"] = "off";
+}
+
+export default config;
